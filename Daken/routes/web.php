7ae5e5f663571c9f3b83e5b9d83695432ff19182
BoDZz98 <abdelrahman1904368@miuegypt.edu.eg>
2022-05-11 16:03:51 +0200

    revert 3

--- Daken/routes/web.php
+++ Daken/routes/web.php
@@ -4,11 +4,27 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\homeController;
 use App\Http\Controllers\orientalC;
 use App\Http\Controllers\productController;
+<<<<<<< HEAD
 use App\Http\Controllers\DashboardController;
+=======
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+>>>>>>> parent of 340b981 (breeze)
 
+// The homepage or the first page
+//              The class we are going to use , name of the function
 Route::get('/',[homeController::class,'index'] )->name('home.index');
 
+<<<<<<< HEAD
 /* Route::get('/', function () {
     return view('welcome');
 }); */
@@ -18,6 +34,9 @@ Route::group(['middleware'=>['auth']],function(){
 
 
 require __DIR__.'/auth.php';
+=======
+Route::get('/contact',[homeController::class,'contact'] )->name('home.contact');
+>>>>>>> parent of 340b981 (breeze)
 
 Route::get('/shop',[homeController::class,'shop'] )->name('home.shop');
 
@@ -29,9 +48,6 @@ Route::get('/shopingCart',[homeController::class,'shopingCart'] )->name('home.sh
 
 Route::get('/checkOut',[homeController::class,'checkOut'] )->name('home.checkOut');
 
-Route::get('/contact',[homeController::class,'contact'] )->name('home.contact');
-
-
 Route::get('/categoryWestern',[homeController::class,'categoryWestern'] )->name('home.categoryWestern');
 Route::get('/categoryOriental',[homeController::class,'categoryOriental'] )->name('home.categoryOriental');
 Route::get('/categorySingle',[homeController::class,'categorySingle'] )->name('home.categorySingle');
@@ -39,3 +55,18 @@ Route::get('/categorySingle',[homeController::class,'categorySingle'] )->name('h
 
 
 Route::resource('products',productController::class);
+// Route::resource('/categoryOriental',oriental::class);
+
+
+/* Route::get('/shop/{category?}/{item?}', function ($category=null,$item=null) {
+    if(isset($category)){
+
+        if(isset($item)){
+            return " <h2>this is item</h2>  {$item}";
+        }
+        return" <h2>this is category</h2>  {$category}";
+       
+    }
+
+    return '<h2>All items</h2>';
+}); */
