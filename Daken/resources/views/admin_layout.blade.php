7983590af3aaa08c9aaf440c434f4d5d4d7b1c34
mohamed ahmed elgendy <mohameddelgendyy@gmail.com>
2022-05-27 22:25:06 +0200

    Update admin_layout.blade.php

--- Daken/resources/views/admin_layout.blade.php
+++ Daken/resources/views/admin_layout.blade.php
@@ -24,6 +24,13 @@
     <link rel="stylesheet" href="{{ url('css/owl.carousel.min.css')}}" type="text/css">
     <link rel="stylesheet" href="{{ url('css/slicknav.min.css')}}" type="text/css">
     <link rel="stylesheet" href="{{ url('css/style.css')}}" type="text/css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    
 </head>
 
 <body>
@@ -179,10 +186,7 @@
                 <div class="footer__widget">
                     <h6>Join Our community Now</h6>
                     <p>Get E-mail updates about our latest shop and special offers.</p>
-                    <form action="#">
-                        <input type="text" placeholder="Enter your mail">
-                        <button type="submit" class="site-btn">Subscribe</button>
-                    </form>
+                   
                     <div class="footer__widget__social">
                         <a href=" https://www.facebook.com/DakenEG"><i class="fa fa-facebook"></i></a>
                         <a href="https://instagram.com/dakeneg?utm_medium=copy_link"><i class="fa fa-instagram"></i></a>
